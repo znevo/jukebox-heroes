44659205701c7cf25b937edb944aada55af1aa80
Zev Nevo <zev@nevo.io>
2022-03-10 20:04:46 -0500

    bulma and meta-detector

--- app/src/main.js
+++ app/src/main.js
@@ -3,10 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './css/main.scss';
+
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+import Metamask from './library/MetaMask';
+const metamask = new Metamask();
+window.metamask = metamask;
+
+metamask.on('EVENT_ACCOUNT_CONNECTED',    () => { window.location.reload() });
+metamask.on('EVENT_ACCOUNT_SWITCHED',     () => { window.location.reload() });
+metamask.on('EVENT_ACCOUNT_DISCONNECTED', () => { window.location.reload() });
+metamask.on('EVENT_CHAIN_SWITCHED',       () => { window.location.reload() });
+
+(async() => {
+  await metamask.init();
+
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})();
