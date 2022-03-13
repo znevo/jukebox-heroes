7fabe6ebf798187cc41dd88f329cd5ddeeb4ffeb
Zev Nevo <zev@nevo.io>
2022-03-12 22:21:34 -0500

    initial buy song page

--- app/src/views/BuySONGView.vue
+++ app/src/views/BuySONGView.vue
@@ -1,4 +1,19 @@
 <template>
   <div class="buy-song">
+    <section class="section">
+      <div class="container is-fluid">
+        <div class="box dapp-box">
+          <h1 class="title is-size-4 has-text-dark">Swap ETH for SONG</h1>
+          <h1 class="subtitle is-size-6 has-text-dark">Redeem your tokens to buy music in the jukebox!</h1>
+           <div class="field" style="position: relative;">
+              <p class="control is-expanded"><input type="text" placeholder="0" class="input is-medium is-rounded"></p>
+              <span class="tag is-rounded is-medium input-song-tag">SONG</span>
+           </div>
+           <div class="has-text-centered">
+              <p class="control"><button class="button is-gradient-button is-medium is-rounded is-fullwidth">Swap</button></p>
+           </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
