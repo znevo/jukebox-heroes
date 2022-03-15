73afcbc941c9e46d9a39ef67793c0160f39b4cb1
Zev Nevo <zev@nevo.io>
2022-03-15 00:18:39 -0400

    openzeppelin, hardhat-etherscan, dotnev

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
 };
